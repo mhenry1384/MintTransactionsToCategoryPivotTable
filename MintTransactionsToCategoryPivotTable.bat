54560597afeb684243d94d35bd11d9bb61050ed8
Matthew Henry <mhenry1384@gmail.com>
2018-03-17 12:46:29 -0400

    Add files via upload

---
+++ MintTransactionsToCategoryPivotTable.bat
@@ -0,0 +1,2 @@
+powershell -Command .\MintTransactionsToCategoryPivotTable.ps1 -file "C:\Users\Matthew\Downloads\transactions.csv" > "C:\Users\Matthew\Downloads\mint.txt"
+"C:\Users\Matthew\Downloads\mint.txt"
---
+++ MintTransactionsToCategoryPivotTable.ps1
@@ -0,0 +1,45 @@
+# Takes a mint.com transactions csv and generates a category pivot table to show you your spending for the current month.
+# The advatage of using this over Mint's reports are that Mint reports don't show subcategories or allow you to filter out categories.
+# Optional parameter: -startDate 1/1/2001
+# Matthew Henry 3/15/2018
+param
+(
+  $startDate = (get-date).tostring("MM/1/yyyy"),
+  $file = "transactions.csv"
+)
+$categoriesToSkip = @("Paycheck", "Transfer")
+echo $startDate
+
+$startDate = [datetime]$startDate
+$endDate = $startDate.AddMonths(1)
+
+$rows = Import-CSV $file
+# Filter by date
+$rows = $rows | Where-Object -FilterScript { ([datetime]$_.Date).Date -ge $startDate -and ([datetime]$_.Date).Date -lt $endDate}
+# Filter out categories
+$rows = $rows | Where-Object -FilterScript { $categoriesToSkip -notcontains $_.Category }
+# Generate pivot table
+$table = @{}
+Foreach ($row IN $rows)
+{
+	$amount = [decimal]$row.Amount
+	if ($row."Transaction Type" -eq "debit")
+	{
+		$amount = -1*$amount
+	}
+	if ($table[$row.Category])
+	{
+		$table[$row.Category] = $amount  + $table[$row.Category]
+	}
+	else
+	{
+		$table[$row.Category] = $amount 
+	}
+}
+$table.GetEnumerator() | sort Value
+$total = 0
+$table.GetEnumerator() | % {$total += $_.Value}
+echo "TOTAL: $total"
+$s = $startDate.tostring('MM/1/yyyy')
+$e = $endDate.tostring('MM/1/yyyy')
+echo "(from $s to $e)"
